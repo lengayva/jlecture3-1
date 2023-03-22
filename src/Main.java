6bda0385da617467f2827e8c5b65a68d42ae1c89
Elena Kovylyaeva <lengayva@yandex.ru>
2023-03-22 22:30:44 +0500

    add zadanie-1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+        // переменная для хранения стоимости билета
+        int bonus = price / 20;
+        // для хранения количества рублей для одной бонусной мили
+
+        System.out.println("Бонус: " + bonus);
+    }
+}
